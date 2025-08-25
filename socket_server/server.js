4a275dee994e194ee1bed13b22d365d23688782f
bravonokoth <bravonokoth@gmail.com>
2025-08-25 21:23:23 +0300

    URL update

--- socket_server/server.js
+++ socket_server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://store-one-olive.vercel.app/', 
         methods: ['GET', 'POST'],
     },
 });
