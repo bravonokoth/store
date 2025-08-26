39eb561ee825b057af7c3cfa2f90915f8772be04
bravonokoth <bravonokoth@gmail.com>
2025-08-26 21:52:20 +0300

    debugging the admin dashboard

--- afront/src/services/api.ts
+++ afront/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://db0f99f15bf7.ngrok-free.app/api';
+const API_BASE_URL = 'https://7672e91733d5.ngrok-free.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
