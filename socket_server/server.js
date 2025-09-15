fbf7b1fa7f3a0cfa7a56720d74b3cfd24d30f9ff
bravonokoth <bravonokoth@gmail.com>
2025-09-15 17:02:07 +0300

    api.ts

--- socket_server/server.js
+++ socket_server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'https://store-one-olive.vercel.app/', 
+        origin: 'https://store-one-olive.vercel.app', 
         methods: ['GET', 'POST'],
     },
 });
