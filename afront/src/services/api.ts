3f62963d1c60d4086ff41dc618aa30a6e877f52a
bravonokoth <bravonokoth@gmail.com>
2025-09-15 15:54:18 +0300

    fixed api.ts

--- afront/src/services/api.ts
+++ afront/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://23132de6fcb9.ngrok-free.app/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
